e1fa611c167505c397b2052f5788dc66d0344e84
mahmoudchemy <realmahmoudcodes@gmail.com>
2025-09-13 13:17:36 +0100

    'implement the hero section'

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,28 @@
+/* eslint-disable no-unused-vars */
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import {heroVideo, smallHeroVideo} from '../utils'
+import { useState } from 'react';
+
 const Hero = () => {
+  const [videoSrc, setVideoSrc]=useState(window, innerWidth<760 ? smallHeroVideo:heroVideo)
+  useGSAP(()=>{
+    gsap.to('#hero',{opacity:1, delay:1.5})
+  },[])
+
   return (
-    <div>Hero</div>
+    <section className="w-full nav-height bg-black relative">
+        <div className="h-5/6 w-full flex-center flex-col">
+            <p id='hero'className="hero-title">Atelier Haytham Mribah</p>
+            <div className='md:w-10/12 w-9/12'>
+              <video>
+                <source src={videoSrc}/>
+              </video>
+            </div>
+        </div>
+    </section>
   )
 }
+
 
 export default Hero
